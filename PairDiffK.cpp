2c9dd5ba4a948cbf7eb80e0b7b8bef2b742d6b3f
Ankur Kumar <109409341+ankurkumar5198@users.noreply.github.com>
2024-10-30 23:28:56 +0530

    Create PairDiffK.cpp

---
+++ PairDiffK.cpp
@@ -0,0 +1,12 @@
+int countPairsWithDiffK(vector<int>& arr, int k) {
+        // code here
+        unordered_map<int,int>mp;
+        int n = arr.size();
+        int ans = 0;
+        for(int  i = 0;i<n;i++) {
+            ans+=mp[arr[i]+k];
+            ans+=mp[arr[i]-k];
+            mp[arr[i]]++;
+        }
+        return ans;
+    }
