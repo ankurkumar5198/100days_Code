d37fd62146bab734e85264ea9d0c3cca3c2dd88f
Ankur Kumar <109409341+ankurkumar5198@users.noreply.github.com>
2024-11-02 23:46:06 +0530

    Create Check.cpp

---
+++ Check.cpp
@@ -0,0 +1,24 @@
+unordered_map<int,int> um;
+        for(int i=0;i<k;i++)
+        {
+            if(um.find(arr[i]) != um.end())
+            {
+                if(um[arr[i]]-i < k)
+                {
+                    return true;
+                }
+            }
+            um[arr[i]]=i;
+        }
+        for(int i=k;i<arr.size();i++)
+        {
+            if(um.find(arr[i]) != um.end())
+            {
+                if(um[arr[i]]-i < k && um[arr[i]]-i>=(-k))
+                {
+                    return true;
+                }
+            }
+            um[arr[i]]=i;
+        }
+        return false;
