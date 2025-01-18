3065b3d45be020dd310755b0075abf2958ed8685
Ankur Kumar <109409341+ankurkumar5198@users.noreply.github.com>
2025-01-18 23:36:52 +0530

    Create ReverseLL.cpp

---
+++ ReverseLL.cpp
@@ -0,0 +1,19 @@
+Node* reverse(Node* head)
+   {
+       if(head==nullptr || head->next==nullptr)
+       return head;
+       
+       Node* node=reverse(head->next);
+       head->next->next=head;
+       head->next=nullptr;
+       return node;
+   }
+    Node* reverseList(struct Node* head) {
+        // code here
+        
+        if(head==nullptr)
+        return head;
+        
+       return reverse(head);
+        // return head;
+    }
