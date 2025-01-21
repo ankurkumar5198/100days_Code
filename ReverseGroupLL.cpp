8530ac70a02a5d57f0f226aaa07c56a7597b2d3c
Ankur Kumar <109409341+ankurkumar5198@users.noreply.github.com>
2025-01-21 23:07:12 +0530

    Create ReverseGroupLL.cpp

---
+++ ReverseGroupLL.cpp
@@ -0,0 +1,20 @@
+Node *reverseKGroup(Node *head, int k) {
+        // code here
+        if(!head || !head->next) return head;
+
+        Node *prev = NULL;
+        Node *curr = head;
+        
+        int it = 0;
+        while(curr && it < k){
+            Node *next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+            it++;
+        }
+        
+        
+        head->next = reverseKGroup(curr,k);
+        return prev;
+    }
